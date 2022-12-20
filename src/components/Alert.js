017e8285cc9806b39561153703fc158e40c30ee3
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2022-12-19 18:46:34 -0800

    add modal messages

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,17 @@
 const Alert = (props) => {
-    return <div className="alert-modal">{props.alert}</div>;
+    return (
+        <div
+            className={`alert-modal ${
+                props.alert.slice(0, 3) === "You"
+                    ? "green"
+                    : props.alert.slice(0, 3) === "Pok"
+                    ? "red"
+                    : "yellow"
+            }`}
+        >
+            {props.alert}
+        </div>
+    );
 };
 
 export default Alert;
